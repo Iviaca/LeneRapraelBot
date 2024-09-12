e0b7fe01b2cfa8232c71b8908d96659b132cfc28
LeenaIviaca <743352170@qq.com>
2024-09-12 23:55:16 +0800

    start of api post coposing

---
+++ BaseClass/PostBase.cs
@@ -0,0 +1,14 @@
+﻿using LeneRapraelBot.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeneRapraelBot.BaseClass
+{
+    internal class PostBase
+    {
+        
+    }
+}
---
+++ Struct/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeneRapraelBot.Struct
+{
+    struct Post
+    {
+        string @action;
+        List<FrozenDictionary<string, string>> @params;
+        string @echo;
+    }
+}
